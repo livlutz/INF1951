c2d670ddfa56d8669ff3ec4a6d58be42dea8fc70
Lívia Lutz <108961867+livlutz@users.noreply.github.com>
2026-03-21 14:04:47 +0000

    DB + Bootstrap links + static dirs

--- isms/isms/isms/settings.py
+++ isms/isms/isms/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = "django-insecure-*x$_l6$431)-tt5p^sgbrpd1i%$yk*^krmvq5!g1f!fuy0l4ai
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -116,3 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
